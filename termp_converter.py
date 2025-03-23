df989d625f443f6ab3f9c94aa301e69a52d4572b
Fl0ris1 <149056628+Fl0ris1@users.noreply.github.com>
2025-03-23 10:36:38 +0100

    Add files via upload

--- termp_converter.py
+++ termp_converter.py
@@ -1,12 +1,23 @@
 from tkinter import *
 import tkinter.font as font
 
+def tempconverter():
+    temp_cel=input_celcius.get()
+    if temp_cel.replace(".","",1).isdigit():
+        lbl_error.place_forget()
+        farenheit=(float(temp_cel)*9/5)+32
+        lbl_output.config(text="The temperature in farenheit is {}".format(farenheit))
+    else:
+        lbl_error.place(x=325,y=80)
+
+
 root=Tk()
 font1=font.Font(family="consolas",weight="bold")
 
 root.geometry("500x250")
 root.title("Temp Converter")
 root.config(background="#99C2A2")
+
 lbl_heading=Label(root,text="Celcius --> Farenheit", font=(font1,15),bg="#99C2A2",fg="#32908F")
 
 lbl_heading.place(x=150,y=15)
@@ -22,8 +33,8 @@ input_celcius.place(x=325,y=63)
 lbl_error=Label(root,text="Please enter a valid input",font=(font1,8),bg="#99C2A2",fg="red")
 
 lbl_output=Label(root,font=(font,12),bg="#99C2A2",fg="#000000")
-lbl_output.place(x=150,y=110)
+lbl_output.place(x=150,y=100)
 
-convert=Button(root,text="Convert temperature",font=(font1,12),bg="#202C39",fg="#B1DDF1",padx=20,pady=10)
+convert=Button(root,text="Convert temperature",font=(font1,12),bg="#537A5A",fg="#B1DDF1",padx=20,pady=10,command=tempconverter)
 convert.place(x=160,y=130)
 root.mainloop()
